03ccb8db1c18ee03c54dc3d22a965e6ff9cfcd18
Mayank Gupta (ARC) <mayank.gupta@iontrading.com>
2019-03-07 16:55:45 +0530

    Initial Application for Testing

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import ParentContainer from './components/ParentContainer';
+
+ReactDOM.render(<ParentContainer />, document.getElementById('root'));
